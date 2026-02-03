d162b12f518cc6cb5f07f9ec6e2c67ad8020ebf9
rosan-f <fikrirausyan131@gmail.com>
2026-02-03 16:40:53 +0700

    feat: add basic middleware setup in Express

--- index.js
+++ index.js
@@ -6,6 +6,32 @@ const PORT = 2000;
 
 app.use(express.json());
 
+app.use((req,res,next) =>{
+  console.log(`A new request received at`+Date.now())
+  console.log("Start")
+
+  res.on('finish',()=>{
+    console.log("End")
+  })
+
+  next()
+})
+
+app.get('/error',(req,res)=>{
+  throw new Error("This is test Error");
+})
+
+app.use((err,req,res,next)=>{
+  console.log(err.message)
+  res.send('Internal Server Error')
+})
+
+app.get('/welcome', (req, res) => {
+  console.log("Middle")
+  res.send("Welcome Rosan")
+})
+
+
 app.get("/", (req, res) => {
   res.send("hello rosan");
 });
